d77554916ba2769cd08b1ff1cd73f752e26c6d5a
Chelsee <Chelsee@Beths-MacBook-Pro.local>
2018-03-16 14:28:32 -0500

    Styles added to AboutUs

--- src/components/About.js
+++ src/components/About.js
@@ -18,94 +18,97 @@ export default class About extends Component {
 
     render() {
         return (
-            <ScrollView style={{ backgroundColor: '#333333' }}>
-                {/* <Image>
+
+            <ImageBackground source={{ uri: 'https://static.tumblr.com/e31f3012fa7c249095a8dddbfc58f0c4/rgmmpty/K3Tmpmf2h/tumblr_static_brick_wall_night_texture_by_kaf94-d373s49.jpg' }} style={styles.container}>
+                <ScrollView>
+                    {/* <Image>
                 source={require('../images/Arvi.jpg')}>
                 </Image> */}
-                <View style={{ backgroundColor: '#333333' }}>
-                    <View style={styles.container}>
+                    <View>
+                        <View style={styles.container}>
 
-                        <Text style={styles.header}>About GigFinder</Text>
+                            <Text style={styles.header}>About GigFinder</Text>
 
-                        <Text style={styles.paragraph}>
-                            GigFinder is a mobile application for connecting bands and individual artists. Its primary function is to help users search for an artist that matches their ideal requirements for a bandmate. Use GigFinder to find a quick replacement for a bandmate who is unable to perform! Users can connect with one another via an instant messenger, manage their profile, search out various artists by Location, Instrument, and Genre.
+                            <Text style={styles.paragraph}>
+                                GigFinder is a mobile application for connecting bands and individual artists. Its primary function is to help users search for an artist that matches their ideal requirements for a bandmate. Use GigFinder to find a quick replacement for a bandmate who is unable to perform! Users can connect with one another via an instant messenger, manage their profile, search out various artists by Location, Instrument, and Genre.
                     </Text>
-                        <Text style={styles.header}>GigFinder Devs</Text>
-                        <Text style={styles.paragraph}>Dylan Commean</Text>
-                        <Text style={styles.paragraph}>Walter Lee</Text>
-                        <Text style={styles.paragraph}>Victor Anaya</Text>
-                        <Text style={styles.paragraph}>Chelsee Arnold</Text>
+                            <Text style={styles.header}>GigFinder Devs</Text>
+                            <Text style={styles.paragraph}>Dylan Commean</Text>
+                            <Text style={styles.paragraph}>Walter Lee</Text>
+                            <Text style={styles.paragraph}>Victor Anaya</Text>
+                            <Text style={styles.paragraph}>Chelsee Arnold</Text>
 
-                        <View style={styles.btncontainer}>
-                            <TouchableOpacity style={styles.buttonContainer}
-                                onPress={() => this.contactUs()}>
-                                <Text style={styles.buttonText}>Contact Us</Text>
-                            </TouchableOpacity >
+                            <View style={styles.btncontainer}>
+                                <TouchableOpacity style={styles.buttonContainer}
+                                    onPress={() => this.contactUs()}>
+                                    <Text style={styles.buttonText}>Contact Us</Text>
+                                </TouchableOpacity >
+                            </View>
                         </View>
                     </View>
-                </View>
 
-            </ScrollView>
-        );
-    }
-}
-
-
+                </ScrollView>
+            </ImageBackground>
+                );
+            }
+        }
+        
+        
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        position: 'relative',
-        borderRadius: 10,
-        marginBottom: 5,
-        backgroundColor: '#333333'
-    },
+                container: {
+                padding: 10,
+                position: 'relative',
+                borderRadius: 10,
+                marginBottom: 5,
+                //backgroundColor: '#333333'
+            },
     header: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'white',
-        marginBottom: 15,
-        marginTop: 10,
-    },
+                fontSize: 20,
+                fontWeight: 'bold',
+                color: 'white',
+                marginBottom: 15,
+                marginTop: 10,
+            },
     paragraph: {
-        color: 'white',
-        fontSize: 16,
-        padding: 10,
-    },
+                color: 'white',
+                fontSize: 16,
+                padding: 10,
+            },
     btncontainer: {
-        flexDirection: 'column',
-    },
+                flexDirection: 'column',
+            },
     input: {
-        height: 40,
-        width: 250,
-        backgroundColor: '#46484c',
-        marginBottom: 5,
-        padding: 5,
-        fontSize: 15,
-        color: 'cyan',
-        position: 'relative',
-        borderRadius: 5,
-        borderColor: 'black',
-        borderWidth: 2,
-        borderBottomWidth: 0,
-        borderRightWidth: 0,
-    },
+                height: 40,
+                width: 250,
+                backgroundColor: '#46484c',
+                marginBottom: 5,
+                padding: 5,
+                fontSize: 15,
+                color: 'cyan',
+                position: 'relative',
+                borderRadius: 5,
+                borderColor: 'black',
+                borderWidth: 2,
+                borderBottomWidth: 0,
+                borderRightWidth: 0,
+            },
     buttonText: {
-        color: '#fff',
-        fontSize: 17,
-        alignSelf: 'center',
-        paddingTop: 3,
-        textAlign: 'center',
-    },
+                    color: '#fff',
+                fontSize: 17,
+                alignSelf: 'center',
+                paddingTop: 3,
+                textAlign: 'center',
+            },
     buttonContainer: {
-        backgroundColor: '#15a3a3',
-        borderColor: '#21ffff',
-        borderWidth: 1,
-        borderRadius: 40,
-        height: 30,
-        width: 120,
-        margin: 5,
-        alignSelf: 'center',
-        borderBottomWidth: 0,
-        borderRightWidth: 0
-    },
-})
+                    backgroundColor: '#15a3a3',
+                borderColor: '#21ffff',
+                borderWidth: 1,
+                borderRadius: 40,
+                height: 30,
+                width: 120,
+                margin: 5,
+                alignSelf: 'center',
+                borderBottomWidth: 0,
+                borderRightWidth: 0
+            },
+        })
